0a82c183872a463ed8fada47734fb3622aaaa5d1
SaurabhApraj <saurabhapraj7@gmail.com>
2021-04-30 07:17:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">Shopping app</a>
+      </header>
+      <main>
+        Product list
+      </main>
+      <footer>
+        &copy; Copyright 2021
+      </footer>
+    </div>
+  );
+}
+
+export default App;
